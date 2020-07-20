2bd101b0cecc3c1b1447d44c3772ec18b06a40f0
duc <duc@bookbotkids.com>
2020-07-20 23:16:53 +0700

    Remove logger service class

---
+++ lib/utils.dart
@@ -0,0 +1,6 @@
+class Utils {
+  static T enumFromString<T>(Iterable<T> values, String value) {
+    return values.firstWhere((type) => type.toString().split(".").last == value,
+        orElse: () => null);
+  }
+}
--- test/logging_service_test.dart
+++ test/logging_service_test.dart
@@ -1,22 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logger/logger.dart';
 import 'package:logging_service/logging_service.dart';
 
 void main() {
   test('logging', () {
-    var logService = LoggingService.shared;
-    Map<String, dynamic> configs = {'env': 'prod', 'loggingLevel': 'debug'};
-    logService.init(configs);
-    var logger = logService.logger;
+    Map<String, dynamic> config = {'env': 'prod', 'loggingLevel': 'debug'};
+    var isLocalEnv = true;
+    var outputs = List<LogOutput>();
+    if (isLocalEnv) {
+      outputs.add(ConsoleOutput());
+    } else {
+      outputs.add(AzureMonitorOutput(config));
+    }
+
+    var logger = Logger(
+        printer: isLocalEnv ? PrettyPrinter() : CloudPrinter(),
+        output: MultipleOutput(outputs));
+
     logger.v("Verbose log");
-
     logger.d("Debug log");
-
     logger.i("Info log");
-
     logger.w("Warning log");
-
     logger.e("Error log");
-
     logger.wtf("What a terrible failure log");
     assert(true);
   });
