0a820b446c8d1e82f1f66d09a2465742f0974caa
duc <duc@bookbotkids.com>
2020-07-20 16:59:09 +0700

    add test cases

--- test/logging_service_test.dart
+++ test/logging_service_test.dart
@@ -1,5 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logging_service/logging_service.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('logging', () {
+    var logService = LoggingService.shared;
+    logService.init({'env': 'dev', 'logLevel': 'debug'});
+    var logger = logService.logger;
+    logger.v("Verbose log");
+
+    logger.d("Debug log");
+
+    logger.i("Info log");
+
+    logger.w("Warning log");
+
+    logger.e("Error log");
+
+    logger.wtf("What a terrible failure log");
+    assert(true);
+  });
 }
