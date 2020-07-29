9ec6dd3a8f516833f580384aa94d1248404c5eaf
duc <duc@bookbotkids.com>
2020-07-29 14:49:35 +0700

    Move LogDatabase into PersistLogOutput

--- lib/outputs/persist_log_output.dart
+++ lib/outputs/persist_log_output.dart
@@ -1,11 +1,15 @@
 import 'package:logger/logger.dart';
-import 'package:logging_service/log_database.dart';
 import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+import 'package:sembast_web/sembast_web.dart';
+import 'package:universal_platform/universal_platform.dart';
+import 'package:uuid/uuid.dart';
+import 'package:sembast/utils/value_utils.dart' as SembastUtils;
 
 /// An abstract output that can persist data
 abstract class PersistLogOutput extends LogOutput {
-  /// The log database instance, share to all output subclasses
-  static LogDatabase _logDatabase = LogDatabase();
+  /// Sembast database instance
+  static Database _database;
 
   /// Create or update log record by `map['id']`
   Future<void> save(Map map, String outputType) async {
@@ -14,12 +18,24 @@ abstract class PersistLogOutput extends LogOutput {
     }
 
     map['_logOutputType'] = outputType;
-    await _logDatabase.save(map);
+    var store = StoreRef.main();
+    var id = map['id'];
+    if (!(id is String && id.isNotEmpty)) {
+      id = Uuid().v4().toString();
+    }
+    if (!map.containsKey('createdAt')) {
+      map['createdAt'] = DateTime.now().millisecondsSinceEpoch;
+    }
+
+    await store.record(id).put(await database, map);
   }
 
   /// Delete log record
   Future<void> remove(String id) async {
-    await _logDatabase.remove(id);
+    final store = StoreRef.main();
+    if (await store.record(id).exists(await database)) {
+      await store.record(id).delete(await database);
+    }
   }
 
   /// List all the logs for special output type, sort by `createdAt asc`
@@ -27,6 +43,29 @@ abstract class PersistLogOutput extends LogOutput {
     var finder = Finder();
     finder.filter = Filter.equals('_logOutputType', type);
     finder.sortOrder = SortOrder('createdAt');
-    return await _logDatabase.query(finder: finder);
+    final store = StoreRef.main();
+    var results = List();
+    var records = await store.find(await database, finder: finder);
+    for (var record in records) {
+      var value = SembastUtils.cloneValue(record.value);
+      results.add(value);
+    }
+
+    return results;
+  }
+
+  /// Get the sembast database instance, init if needed
+  static Future<Database> get database async {
+    if (_database == null) {
+      dynamic databaseFactory;
+      if (UniversalPlatform.isWeb) {
+        databaseFactory = databaseFactoryWeb;
+      }
+
+      databaseFactory = databaseFactoryIo;
+      _database = await databaseFactory.openDatabase('log.db');
+    }
+
+    return _database;
   }
 }
